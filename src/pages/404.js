ccfaca22d18cd2e0b3e5d454906904e97e566071
Debashis Barman <debashisb04@gmail.com>
2018-02-19 11:34:29 +0530

    Added a custom 404, 301 for Sofia and other metadata

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default () => (
+  <div className="db-site">
+    <main>
+      <div className="grid">
+        <div className="row">
+          <div className="col">
+            <h3>404. That's an error.</h3>
+            <p className="lead">The requested URL was not found on this server. That's all we know.</p>
+            <p>Go back to <a href="/">debashisbarman.in</a>.</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+)
---
+++ src/pages/Sofia.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default () => (
+  <div className="db-site">
+    <main>
+      <div className="grid">
+        <div className="row">
+          <div className="col">
+            <h3>301. Permanently moved.</h3>
+            <p className="lead">The requested URL is permanently moved to <a href="https://medium.com/@DebashisBarman/creating-a-twitter-bot-with-node-js-bea760b80bd5" target="_blank" rel="nofollow noreferrer">this location</a>. That's all we know.</p>
+            <p>Go back to <a href="/">debashisbarman.in</a>.</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+)
